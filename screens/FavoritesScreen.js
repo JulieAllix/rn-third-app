d3f9ddd755b0e4ac54f470b216a2caf6792bcdbb
JulieAllix <allixjulie@gmail.com>
2020-06-27 15:28:49 +0200

    Adding Filtering Logic

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 
+
 import MealList from '../components/MealList';
 import DefaultText from '../components/DefaultText';
 
